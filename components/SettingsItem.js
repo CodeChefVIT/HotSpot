f9e798eebffa66d8cfb191638b34cf24cd02b0ae
Sarthak <sarthakbh321@gmail.com>
2020-04-22 22:47:52 +0530

    added setting icons

--- components/SettingsItem.js
+++ components/SettingsItem.js
@@ -1,10 +1,24 @@
 import React from "react"
 import {View, StyleSheet, Text, TouchableOpacity} from "react-native"
+import {SimpleLineIcons, Entypo} from '@expo/vector-icons'
 
 function SettingsItem(props) {
+    let icon = null
+    if(props.name === "Display") {
+        icon = <SimpleLineIcons name="screen-smartphone" size={25} color="#888" />
+    } else if(props.name === "Help") {
+        icon = <Entypo name="help" size={25} color="#888" />
+    } else if(props.name === "Permissions") {
+        icon = <Entypo name="book" size={25} color="#888" />
+    } else if(props.name === "About") {
+        icon = <SimpleLineIcons name="user" size={25} color="#888" />
+    }
+
+    
     return (
         <TouchableOpacity style={styles.container}>
-            <View>
+            <View style={styles.row}>
+                {icon}
                 <Text style={styles.text}>{props.name}</Text>
             </View>
         </TouchableOpacity>
@@ -23,5 +37,10 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 20,
-    }
+        paddingLeft: 10,
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
 })
